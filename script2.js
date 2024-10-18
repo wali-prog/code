bab8ed70bc864bdbf725fab116a39fba99f6384b
wali-prog <noorwaliya647@gmail.com>
2024-10-18 20:22:25 +0500

    updated the files

---
+++ script2.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100;
+  document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+window.onload = function () {
+  showSlide(currentSlide);
+};
